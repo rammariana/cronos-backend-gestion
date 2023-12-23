026da7baa6215630780a3daf3a34fb43ca2ff8a6
Mariana <marianasmatos1@gmail.com>
2023-12-22 20:12:24 -0500

    change date in userSchema

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -26,7 +26,7 @@ const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  date: { type: Date, default: new Date().toLocaleDateString() },
 });
 
 const taskSchema = new Schema({
